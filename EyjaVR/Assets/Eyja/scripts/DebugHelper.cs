bf1c3f04e481dd794b043408646eda160c87c5a2
Theo Winter <theo.winter@gmail.com>
2020-08-08 21:46:38 +0200

    Added support for roomscale movement, added respawn logic when player falls off island,

---
+++ EyjaVR/Assets/Eyja/scripts/DebugHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugHelper : MonoBehaviour
+{
+
+    public static DebugHelper Instance;
+
+    bool inMenu;
+
+    Text logText;
+
+    void Awake()
+    {
+        Instance = this; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rt = DebugUIBuilder.instance.AddLabel("Debug");
+        logText = rt.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            else DebugUIBuilder.instance.Show();
+            inMenu = !inMenu;
+        }
+    }
+
+    public void Log(string msg)
+    {
+        if(logText != null){
+            logText.text = msg;
+        }
+    }
+}
