1a7c5ab2849d821fde001c6c3941e5366421c558
Theo Winter <theo.winter@gmail.com>
2020-08-14 15:37:44 +0200

    Added burnable script that can be triggerd by touch the cube with a torch. Moved particles systems to the game objects themselves since this makes it easier to reference them

---
+++ EyjaVR/Assets/Eyja/scripts/Burnable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burnable : MonoBehaviour
+{
+    public Boolean isBurning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isBurning)
+        {
+            GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Burnable burnableGameObject = other.gameObject.GetComponent<Burnable>();
+        if (burnableGameObject != null && burnableGameObject.isBurning)
+        {
+            print("Object has been triggered to start burning");
+            isBurning = true;
+            GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
