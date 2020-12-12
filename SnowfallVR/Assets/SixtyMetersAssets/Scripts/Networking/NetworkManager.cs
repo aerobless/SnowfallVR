ae744cb4e97d8ed66bb0a3dff7de5d6b333f1926
Theo Winter <theo.winter@gmail.com>
2020-12-12 14:47:33 +0100

    Added rudimentary multiplayer support with photon. Player is represented by a sphere that can be seen to move.

---
+++ SnowfallVR/Assets/SixtyMetersAssets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using Photon.Pun.Demo.Cockpit;
+using Photon.Realtime;
+using SixtyMetersAssets.Characters.Player;
+using UnityEngine;
+
+namespace SixtyMetersAssets.Scripts.Networking
+{
+    public class NetworkManager : MonoBehaviourPunCallbacks
+    {
+        private const string DefaultRoom = "SnowfallVR_Default";
+
+        private string _selectedRoom;
+        private Transform _localPlayerTransform;
+
+        private void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+            {
+                _localPlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+
+            if (PhotonNetwork.IsConnected)
+            {
+                OnConnectedToMaster();
+                Debug.Log("Already connected");
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = Application.version;
+                Debug.Log("Connecting...");
+            }
+
+            _selectedRoom = DefaultRoom;
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+            Debug.Log("Connected to master!");
+            Debug.Log("Joining room: " + _selectedRoom);
+
+            JoinOrCreateRoom(_selectedRoom);
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            base.OnJoinRoomFailed(returnCode, message);
+            Debug.LogWarning("Failed to join room " + _selectedRoom + " " + message);
+
+            // TODO: retry? needs logic to stop after a few retries
+            //JoinOrCreateRoom(_selectedRoom);
+        }
+
+        private void JoinOrCreateRoom(string roomName)
+        {
+            Debug.Log("Creating room " + roomName);
+            PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions {MaxPlayers = 4, IsOpen = true, IsVisible = true},
+                TypedLobby.Default);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined room!");
+
+            CreatePlayer();
+        }
+        
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            base.OnRoomListUpdate(roomList);
+            Debug.Log("Got " + roomList.Count + " rooms.");
+            foreach(RoomInfo room in roomList)
+            {
+                Debug.Log("Room: " + room.Name + ", " + room.PlayerCount);
+            }
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+            //OnPlayersChanged?.Invoke(); //TODO:
+            Debug.Log("A new player enter the room with id" + newPlayer.UserId);
+        }
+        
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            base.OnPlayerLeftRoom(otherPlayer);
+            //OnPlayersChanged?.Invoke(); //TODO:
+
+        }
+
+        private void CreatePlayer()
+        {
+            PhotonNetwork.Instantiate(Path.Combine("NetworkPrefabs", "NetworkHead"), _localPlayerTransform.position,
+                Quaternion.identity, 0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ SnowfallVR/Assets/SixtyMetersAssets/Scripts/Networking/PhotonViewParentableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PhotonViewParentableObject : MonoBehaviour
+{
+
+    public string parentPath_vr = "";
+    public string alternativePath = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParentObject(parentPath_vr);
+    }
+
+    private void ParentObject(string path)
+    {
+        var photonView = GetComponent<PhotonView>();
+        var parent = GameObject.Find(photonView.IsMine ? path : alternativePath);
+        
+        Assert.IsNotNull(parent, "Photon view " + gameObject.name + " has an invalid parent gameobject path " + path);
+
+        transform.parent = parent.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+}
